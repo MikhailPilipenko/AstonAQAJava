e951b16e172148a9426c06599b8b90a4b514b43f
MikhailPilipenko <pmgomel@mail.ru>
2023-12-11 23:30:51 +0300

    create class Lesson_11_maven

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Project using Maven with archetype quickstart");
+        System.out.println("Added dependencies JUnit 5 , Selenium");
+    }
+}
