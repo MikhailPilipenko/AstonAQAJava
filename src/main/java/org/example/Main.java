f08fca606cedee66aee1fe487f2db228155ff70a
MikhailPilipenko <pmgomel@mail.ru>
2023-12-11 23:48:09 +0300

    create class Lesson_11_maven

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Project using Gradle");
+        System.out.println("Added dependencies JUnit 5 , Selenium");
+    }
+}
