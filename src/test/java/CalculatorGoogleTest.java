f7c7fa3059f9ffa768172cdda15cc16b70869d27
MikhailPilipenko <pmgomel@mail.ru>
2024-01-04 23:09:23 +0300

    change class CalculatorGoogleTest Lesson_17

--- src/test/java/CalculatorGoogleTest.java
+++ src/test/java/CalculatorGoogleTest.java
@@ -14,18 +14,13 @@ public class CalculatorGoogleTest {
 
     @BeforeClass
     public void initialize() {
-        //AndroidDriver driver;
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
-//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 7 API 30");
         capabilities.setCapability("deviceName", "Pixel 7 API 30");
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("automationName", "UiAutomator2");
         capabilities.setCapability("appPackage", "com.google.android.calculator");
         capabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
-//        capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.google.android.calculator");
-//        capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "cal.CalculatorActivity");
-//        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
 
         try {
             driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
@@ -36,20 +31,51 @@ public class CalculatorGoogleTest {
     }
 
     @Test
-    public void checkCalculate() {
+    public void checkAddition() {
         driver.findElement(By.id("com.google.android.calculator:id/digit_9")).click();
-//        driver.findElementById("com.google.android.calculator:id/digit_9").click();
         driver.findElement(By.id("com.google.android.calculator:id/op_add")).click();
-//        driver.findElementById("com.google.android.calculator:id/op_add").click();
         driver.findElement(By.id("com.google.android.calculator:id/digit_5")).click();
-//        driver.findElementById("com.google.android.calculator:id/digit_5").click();
         driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
-//        driver.findElementById("com.google.android.calculator:id/eq").click();
 
-//        driver.findElement(By.id("com.google.android.calculator:id/result_final")).click();
         String result = driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText();
 
         Assert.assertEquals(result, "14");
+    }
+
+    @Test
+    public void checkSubstraction() {
+        driver.findElement(By.id("com.google.android.calculator:id/digit_8")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_sub")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_3")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+        String result = driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText();
+
+        Assert.assertEquals(result, "5");
+    }
+
+    @Test
+    public void checkDivision() {
+        driver.findElement(By.id("com.google.android.calculator:id/digit_6")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_div")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_2")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+        String result = driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText();
+
+        Assert.assertEquals(result, "3");
+    }
+
+    @Test
+    public void checkMultiplication() {
+        driver.findElement(By.id("com.google.android.calculator:id/digit_4")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/op_mul")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/digit_7")).click();
+        driver.findElement(By.id("com.google.android.calculator:id/eq")).click();
+
+        String result = driver.findElement(By.id("com.google.android.calculator:id/result_final")).getText();
+
+        Assert.assertEquals(result, "28");
     }
 
     @AfterClass
